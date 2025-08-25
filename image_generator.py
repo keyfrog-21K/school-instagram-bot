e8e6e8a1c88927c61a76611c29ccebdbda56600f
keyfrog-21K <sp.keyfrog@gmail.com>
2025-08-25 17:22:08 +0900

    feat: 이미지 생성 함수 작성

---
+++ image_generator.py
@@ -0,0 +1,55 @@
+# image_generator.py
+from PIL import Image, ImageDraw, ImageFont
+
+class drawTextOnBackground:
+    def __init__(self, background_path, font_path, title, body_lines):
+        self.img = Image.open(background_path).convert("RGBA")
+        self.W, self.H = self.img.size
+        self.draw = ImageDraw.Draw(self.img)
+
+        # 제목 / 본문 폰트
+        self.title_font = ImageFont.truetype(font_path, 80)
+        self.body_font = ImageFont.truetype(font_path, 50)  # 본문 글자 조금 더 크게
+
+        self.title = title
+        self.body_lines = body_lines
+        self.fill_color = "black"  # 글자 색상 통일
+        self.line_spacing = 60     # 본문 줄간격
+        self.title_body_spacing = 50  # 제목과 본문 사이 간격
+
+    def draw_centered_text(self, text, y, font):
+        """가로 중앙 정렬 텍스트 그리기"""
+        bbox = self.draw.textbbox((0, 0), text, font=font)
+        width = bbox[2] - bbox[0]
+        self.draw.text(((self.W - width) / 2, y), text, font=font, fill=self.fill_color)
+
+    def render(self, output_path="output.png"):
+        # 제목 높이 계산
+        title_bbox = self.draw.textbbox((0, 0), self.title, font=self.title_font)
+        title_height = title_bbox[3] - title_bbox[1]
+
+        # 본문 전체 높이 계산
+        body_heights = sum([self.draw.textbbox((0, 0), line, font=self.body_font)[3] -
+                            self.draw.textbbox((0, 0), line, font=self.body_font)[1]
+                            for line in self.body_lines])
+        total_body_height = body_heights + (len(self.body_lines)-1) * self.line_spacing
+
+        # 전체 텍스트 높이
+        total_text_height = title_height + self.title_body_spacing + total_body_height
+
+        # 상하 중앙 기준 y 시작 위치
+        start_y = (self.H - total_text_height) / 2
+
+        # 제목 그리기
+        self.draw_centered_text(self.title, start_y, self.title_font)
+
+        # 본문 그리기
+        current_y = start_y + title_height + self.title_body_spacing
+        for line in self.body_lines:
+            self.draw_centered_text(line, current_y, self.body_font)
+            line_bbox = self.draw.textbbox((0,0), line, font=self.body_font)
+            line_height = line_bbox[3] - line_bbox[1]
+            current_y += line_height + self.line_spacing
+
+        # 이미지 저장
+        self.img.save(output_path)
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 # main.py
 from getapi import Parser
+from image_generator import drawTextOnBackground
 from datetime import datetime
 
 
 def main():
     officeCode = 'R10' # 경상북도교육청
     schoolCode = '8801139' # 구미중학교
-    today = datetime.today().strftime("%Y%m%d")
+    background_path = './src/background.png'
+    font_path = './src/WantedSans-SemiBold.ttf'
 
-    parser = Parser(officeCode, schoolCode, today)
+    today = datetime.today()
+
+    parser = Parser(officeCode, schoolCode, today.strftime("%Y%m%d"))
     json = parser.get_data()
 
     row = json["mealServiceDietInfo"][1]["row"][0]
     dish_raw = row["DDISH_NM"]
     meals = [dish.strip() for dish in dish_raw.split("<br/>") if dish.strip()]
+
+    draw = drawTextOnBackground(background_path, font_path, today.strftime("%Y년 %m월 %d일"), meals)
+    draw.render()
+
+if __name__ == '__main__':
+    main()
