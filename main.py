df8793befa96ab86745e4e93076ac13c2c41fdb2
keyfrog-21K <sp.keyfrog@gmail.com>
2025-08-25 17:09:22 +0900

    feat: main.py에서 Parser 클래스를 통해 급식 정보를 받아오는 코드 작성

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # main.py
-print('Hello world!')
+from getapi import Parser
+from datetime import datetime
+
+
+def main():
+    officeCode = 'R10' # 경상북도교육청
+    schoolCode = '8801139' # 구미중학교
+    today = datetime.today().strftime("%Y%m%d")
+
+    parser = Parser(officeCode, schoolCode, today)
+    json = parser.get_data()
+
+    row = json["mealServiceDietInfo"][1]["row"][0]
+    dish_raw = row["DDISH_NM"]
+    meals = [dish.strip() for dish in dish_raw.split("<br/>") if dish.strip()]
