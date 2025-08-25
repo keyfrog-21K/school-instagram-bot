a5f104a78d91e099e691c293fb2b2eae71dc768b
keyfrog-21K <sp.keyfrog@gmail.com>
2025-08-25 16:45:17 +0900

    feat: 급식 정보를 요청할 파서 작성

--- getapi.py
+++ getapi.py
@@ -1,2 +1,26 @@
 # getapi.py
 # API를 불러오는 코드
+import os
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+api_key = os.getenv('NEIS_APIKEY')
+
+
+class Parser:
+    def __init__(self, officeCode, schoolCode, ymd):
+        self.officeCode = officeCode
+        self.schoolCode = schoolCode
+        self.ymd = ymd
+        self.url = 'https://open.neis.go.kr/hub/mealServiceDietInfo'
+        self.params = {
+            'KEY': api_key,
+            'Type': 'json',
+            'ATPT_OFCDC_SC_CODE': self.officeCode,
+            'SD_SCHUL_CODE': self.schoolCode
+        }
+
+    def get_data(self):
+        response = requests.get(self.url, params=self.params)
+        return response.json()
